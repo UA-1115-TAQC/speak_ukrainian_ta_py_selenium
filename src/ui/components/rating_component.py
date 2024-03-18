52c4627907c3f98877545d99520d2b9421a92647
Alevtyna Parysta <luxor952@gmail.com>
2024-03-18 19:03:57 +0100

    add get_rating method

--- src/ui/components/rating_component.py
+++ src/ui/components/rating_component.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.remote.webelement import WebElement
 from src.ui.components.base_component import BaseComponent
+from selenium.webdriver.common.by import By
+
+ZERO_STAR_RATE = (By.XPATH, "./descendant::li[@class='ant-rate-star ant-rate-star-zero']")
+FULL_STAR_RATE = (By.XPATH, "./descendant::li[@class='ant-rate-star ant-rate-star-full']")
+HALF_STAR_RATE = (By.XPATH, "./descendant::li[@class='ant-rate-star ant-rate-star-half']")
 
 
 class RatingComponent(BaseComponent):
 
     def __init__(self, node: WebElement) -> None:
         super().__init__(node)
-

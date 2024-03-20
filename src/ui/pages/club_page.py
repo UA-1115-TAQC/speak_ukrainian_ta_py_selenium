830664786fc0f519c36d46231bc487d0e9ebf724
Alevtyna Parysta <luxor952@gmail.com>
2024-03-20 16:39:48 +0100

    upd club_page, added sign_up_to_club

--- src/ui/pages/club_page.py
+++ src/ui/pages/club_page.py
@@ -1,13 +1,18 @@
-from src.ui.pages.base_page import BasePage
+from src.ui.pages.base_pages import base_page
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from src.ui.components.rating_component import RatingComponent
 
-
 RATING = (By.XPATH, "./descendant::ul[@role='radiogroup']")
+SIGN_UP_TO_CLUB = (By.XPATH, "./descendant::span[text()='Записатись на гурток']")
+WRITE_TO_MANAGER = (By.XPATH, "./descendant::span[text()='Написати менеджеру']")
+CLUB_DESCRIPTION = (By.XPATH, "./descendant::div[@class='content']")
+CLUB_NAME = (By.XPATH, "./descendant::span[@class='club-name']")
+CLUB_COVER = (By.XPATH, ".//header[contains(@class,'page-header')]")
 
 
-class ClubPage(BasePage):
+
+class ClubPage(base_page):
 
     def __init__(self, driver: WebDriver) -> None:
         super().__init__(driver)

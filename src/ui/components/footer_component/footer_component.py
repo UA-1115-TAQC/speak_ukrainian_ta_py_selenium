bb8d1f7c853f81ddd1912a6f5f4a2153b7136ad8
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-03-27 23:19:25 +0200

    Refactoring footer and added TUA-982

--- src/ui/components/footer_component/footer_component.py
+++ src/ui/components/footer_component/footer_component.py
@@ -1,6 +1,7 @@
+from typing import Self
+
 from selenium.webdriver.remote.webelement import WebElement
 from src.ui.components.base_component import BaseComponent
-from typing import Self
 
 
 class FooterComponent(BaseComponent):
@@ -15,15 +16,14 @@ class FooterComponent(BaseComponent):
             "sponsors_links": ("xpath", "//div[contains(@class,'sponsors')]/a[contains(@href, 'https')]"),
             "donate_title": ("xpath", "//div[@class='footer-donate']/div[@class='article']"),
             "donate_explanation": ("xpath", "//div[@class='desc']"),
-            "donate_button": ("xpath", "//button[contains(@class,'donate-button')]")
-
+            "donate_button": ("xpath", "//button[contains(@class,'donate-button')]"),
+            "social_block": ("xpath", "//div[@class='footer-social']"),
+            "sponsors_block": ("xpath", "//div[@class='footer-partners']"),
+            "donate_block": ("xpath", "//div[@class='footer-donate']")
         }
 
-    def click_on_logo(self) -> None:
-        return self.logo.click()
-
-    def get_motto_under_logo_text(self) -> str:
-        return self.motto_under_logo.text()
+    def click_on_logo(self) -> Self:
+        return self.logo.click
 
     @property
     def list_of_social_links(self) -> list[WebElement]:
@@ -32,15 +32,6 @@ class FooterComponent(BaseComponent):
     def get_social_links(self) -> list[str]:
         return [link.get_attribute("href") for link in self.list_of_social_links]
 
-    def get_copyright_text(self) -> str:
-        return self.copyright_text.text
-
-    def get_sponsors_title_text(self) -> str:
-        return self.sponsors_title.text
-
-    def get_sponsors_title_attribute(self, name: str) -> str:
-        return self.sponsors_title.get_attribute(name)
-
     @property
     def list_of_sponsors_links(self) -> list[WebElement]:
         return self.node.find_elements(*self.locators["sponsors_links"])
@@ -48,11 +39,5 @@ class FooterComponent(BaseComponent):
     def get_sponsors_links(self) -> list[str]:
         return [link.get_attribute("href") for link in self.list_of_sponsors_links]
 
-    def get_donate_title_text(self) -> str:
-        return self.donate_title.text()
-
-    def get_donate_explanation_text(self) -> str:
-        return self.donate_explanation.text()
-
-    def click_on_donate_button(self):
-        return self.donate_button.click()
+    def click_on_donate_button(self) -> Self:
+        return self.donate_button.click

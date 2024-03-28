433b636b380c066ffcb8c3586402a7414a673ad0
Anastasiia <anastasiiak55@gmail.com>
2024-03-28 02:31:45 +0200

    change to page factory, refactored

--- src/ui/components/club_card_component.py
+++ src/ui/components/club_card_component.py
@@ -1,95 +1,37 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from src.ui.components.base_component import BaseComponent
-from selenium.webdriver.common.by import By
-
 from src.ui.components.club_info_popup import ClubInfoPopup
 from src.ui.elements.direction_element import DirectionElement
-
-LOGO = (By.XPATH, ".//div[@class='title']//img")
-NAME = (By.XPATH, ".//div[contains(@class,'name')]")
-DIRECTIONS_WEB_ELEMENT = (By.XPATH, ".//span[contains(@class,'ant-tag')]")
-DESCRIPTION = (By.XPATH, ".//p[contains(@class,'description')]")
-RATING = (By.XPATH, ".//ul[contains(@class,'rating')]")
-ADDRESS = (By.XPATH, ".//div[contains(@class,'address')]")
-ADDRESS_LOCATION_NAME = (By.XPATH, "//div[contains(@class,'address')]/span[contains(@class,'text')]")
-ONLINE = (By.XPATH, "./descendant::div[@class='club-online']")
-DETAILS_BUTTON = (By.XPATH, ".//*[contains(@class,'details-button')]")
-POPUP_WEB_ELEMENT = (By.XPATH, "//div[@class='ant-modal-root css-1kvr9ql']")
 
 
 class ClubCardComponent(BaseComponent):
 
     def __init__(self, driver, node):
         super().__init__(node)
+        self.locators = {
+            "logo": ("xpath", ".//div[@class='title']//img"),
+            "name": ("xpath", ".//div[contains(@class,'name')]"),
+            "directions": ("xpath", ".//span[contains(@class,'ant-tag')]"),
+            "description": ("xpath", ".//p[contains(@class,'description')]"),
+            "rating": ("xpath", ".//ul[contains(@class,'rating')]"),
+            "address": ("xpath", ".//div[contains(@class,'address')]"),
+            "address_location_name": ("xpath", "//div[contains(@class,'address')]/span[contains(@class,'text')]"),
+            "online": ("xpath", "./descendant::div[@class='club-online']"),
+            "details_button": ("xpath", ".//*[contains(@class,'details-button')]"),
+            "popup": ("xpath", "//div[@class='ant-modal-root css-1kvr9ql']"),
+            "club_info_popup_root": ("xpath", "//div[contains(@class,'clubInfo')]"),
+        }
         self._driver = driver
         self._wait = WebDriverWait(self._driver, 10)
-        self._logo = None
-        self._name = None
-        self._direction_list = None
-        self._description = None
-        self._rating = None
-        self._address = None
-        self._address_location_name = None
-        self._online = None
-        self._details_button = None
-
-    @property
-    def logo(self):
-        if not self._logo:
-            self._logo = self.node.find_element(*LOGO)
-        return self._logo
-
-    @property
-    def name(self):
-        if not self._name:
-            self._name = self.node.find_element(*NAME)
-        return self._name
 
     @property
     def direction_list(self):
-        if not self._direction_list:
-            self._direction_list = []
-            directions = self.node.find_elements(*DIRECTIONS_WEB_ELEMENT)
-            for direction in directions:
-                self._direction_list.append(DirectionElement(direction))
-        return self._direction_list
-
-    @property
-    def description(self):
-        if not self._description:
-            self._description = self.node.find_element(*DESCRIPTION)
-        return self._description
-
-    @property
-    def rating(self):
-        if not self._rating:
-            self._rating = self.node.find_element(*RATING)
-        return self._rating
-
-    @property
-    def address(self):
-        if not self._address:
-            self._address = self.node.find_element(*ADDRESS)
-        return self._address
-
-    @property
-    def address_location_name(self):
-        if not self._address_location_name:
-            self._address_location_name = self.node.find_element(*ADDRESS_LOCATION_NAME)
-        return self._address_location_name
-
-    @property
-    def online(self):
-        if not self._online:
-            self._online = self.node.find_element(*ONLINE)
-        return self._online
-
-    @property
-    def details_button(self):
-        if not self._details_button:
-            self._details_button = self.node.find_element(*DETAILS_BUTTON)
-        return self._details_button
+        direction_list = []
+        directions = self.node.find_elements(*self.locators["directions"])
+        for direction in directions:
+            direction_list.append(DirectionElement(direction))
+        return direction_list
 
     def get_logo_src(self):
         self.logo.get_attribute("src")
@@ -101,7 +43,7 @@ class ClubCardComponent(BaseComponent):
         return text.lower() in self.get_name_text().lower()
 
     def direction_contains(self, text):
-        for direction in self._direction_list:
+        for direction in self.direction_list:
             if text.lower() in direction.get_name_text().lower():
                 return True
         return False
@@ -111,8 +53,8 @@ class ClubCardComponent(BaseComponent):
 
     def click_title(self):
         self.name.click()
-        self._wait.until(ec.presence_of_element_located(POPUP_WEB_ELEMENT))
-        return ClubInfoPopup(self._driver)
+        self._wait.until(ec.presence_of_element_located(self.locators["popup"]))
+        return ClubInfoPopup(self._driver.find_element(*self.locators["club_info_popup_root"]))
 
     def click_address(self):
         self.address.click()
--- src/ui/pages/clubs_page.py
+++ src/ui/pages/clubs_page.py
@@ -6,85 +6,72 @@ from src.ui.components.list_control_component import ListControlComponent
 from src.ui.components.pagination_component import PaginationComponent
 from src.ui.components.search_sider_component import SearchSiderComponent
 from src.ui.pages.base_pages.base_page import BasePage
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-
-SEARCH_CLUBS_HEADER_WEB_ELEMENT = (By.XPATH, "//div[contains(@class, 'lower-header-box')]")
-PAGINATION_WEB_ELEMENT = (By.XPATH, "//ul[contains(@class,'ant-pagination') and contains(@class,'pagination')]")
-SEARCH_SIDER_WEB_ELEMENT = (By.XPATH, "//div[contains(@class, 'sider')]")
-LIST_CONTROL_WEB_ELEMENT = (By.XPATH, "//div[contains(@class, 'club-list-control')]")
-CLUBS_CARD_WEB_ELEMENT = (By.XPATH, "//div[contains(@class,'content-clubs-list')]/child::div")
-CENTER_CARD_WEB_ELEMENT = (By.XPATH, "//div[contains(@class,'content-center-list')]/child::div")
 
 
 class ClubsPage(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._wait = WebDriverWait(self._driver, 10)
-        self._search_clubs_header = None
-        self._pagination = None
-        self._search_sider = None
-        self._list_control = None
-        self._club_card_list = None
-        self._center_card_list = None
-        self.select_what_cards_to_show()
+        self.locators = {
+            **self.locators,
+            "search_clubs_header": ("xpath", "//div[contains(@class, 'lower-header-box')]"),
+            "pagination": ("xpath", "//ul[contains(@class,'ant-pagination') and contains(@class,'pagination')]"),
+            "search_sider": ("xpath", "//div[contains(@class, 'sider')]"),
+            "list_control": ("xpath", "//div[contains(@class, 'club-list-control')]"),
+            "club_cards": ("xpath", "//div[contains(@class,'content-clubs-list')]/child::div"),
+            "center_cards": ("xpath", "//div[contains(@class,'content-center-list')]/child::div"),
+        }
+        self._driver = driver
+        self._wait = WebDriverWait(self._driver, 30)
 
     @property
     def search_clubs_header(self):
-        if not self._search_clubs_header:
-            self._search_clubs_header = AdvancedSearchClubsHeaderComponent(self.driver, self._driver.find_element(*SEARCH_CLUBS_HEADER_WEB_ELEMENT))
-        return self._search_clubs_header
+        return AdvancedSearchClubsHeaderComponent(self._driver, self._driver.find_element(*self.locators["search_clubs_header"]))
 
     @property
     def pagination(self):
-        if not self._pagination:
-            self._pagination = PaginationComponent(self._driver, self._driver.find_element(*PAGINATION_WEB_ELEMENT))
-        return self._pagination
+        return PaginationComponent(self._driver, self._driver.find_element(*self.locators["pagination"]))
 
     @property
     def search_sider(self):
-        if not self._search_sider:
-            self._search_sider = SearchSiderComponent(self.driver, self._driver.find_element(*SEARCH_SIDER_WEB_ELEMENT))
-        return self._search_sider
+        return SearchSiderComponent(self.driver, self._driver.find_element(*self.locators["search_sider"]))
 
     @property
     def list_control(self):
-        if not self._list_control:
-            self._list_control = ListControlComponent(self._driver.find_element(*LIST_CONTROL_WEB_ELEMENT))
-        return self._list_control
+        return ListControlComponent(self._driver.find_element(*self.locators["list_control"]))
 
     @property
-    def club_card_list(self):
-        if not self._club_card_list:
-            self._club_card_list = []
-            club_elements_list = self._driver.find_elements(*CLUBS_CARD_WEB_ELEMENT)
-            for club in club_elements_list:
-                self._club_card_list.append(ClubCardComponent(self._driver, club))
-        return self._club_card_list
-
-    @property
-    def center_card_list(self):
-        if not self._center_card_list:
-            self._center_card_list = []
-            center_elements_list = self._driver.find_elements(*CENTER_CARD_WEB_ELEMENT)
-            for center in center_elements_list:
-                self._center_card_list.append(CenterCardComponent(center))
-        return self._center_card_list
-
-    def select_what_cards_to_show(self):
+    def card_list(self):
         if self.search_sider_is_opened() and self.search_sider.checked_radio_button.get_attribute("innerText") == "Центр":
-            self.center_card_list
+            return self.get_center_card_list()
         else:
-            self.club_card_list
+            return self.get_club_card_list()
+
+    def get_club_card_list(self):
+        club_card_list = []
+        club_elements_list = self._driver.find_elements(*self.locators["club_cards"])
+        for club in club_elements_list:
+            club_card_list.append(ClubCardComponent(self._driver, club))
+        return club_card_list
+
+    def get_center_card_list(self):
+        center_card_list = []
+        center_elements_list = self._driver.find_elements(*self.locators["center_cards"])
+        for center in center_elements_list:
+            center_card_list.append(CenterCardComponent(center))
+        return center_card_list
 
     def search_sider_is_opened(self):
         try:
-            self._driver.find_element(*SEARCH_SIDER_WEB_ELEMENT)
+            self._driver.find_element(*self.locators["search_sider"])
         except NoSuchElementException:
             return False
         return True
+
+    def is_card_list_empty(self):
+        return len(self.card_list) == 0
 
     def wait_until_clubs_page_is_loaded(self, timeout=30):
         self._wait.until(ec.url_contains("clubs"))

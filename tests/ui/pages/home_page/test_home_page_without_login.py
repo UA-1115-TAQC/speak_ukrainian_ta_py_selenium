a515b418bb0fcdeb06b252746d0e7fde9e57b50f
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-04-02 13:14:18 +0300

    Refactoring

--- tests/ui/pages/home_page/test_home_page_without_login.py
+++ tests/ui/pages/home_page/test_home_page_without_login.py
@@ -10,4 +10,6 @@ class TestHomePageWithoutLogin(BaseTestRunner):
         self.homepage.scroll_to_all_clubs_button().carousel_card_component.click_carousel_card_all_clubs_button()
         self.assertTrue(self.driver.current_url.__contains__("clubs"), "URL for clubs page are different")
 
-   
+    def test_check_that_slick_dots_container_on_img_carousel_is_centered(self):
+        self.assertIn("center",
+                      self.homepage.carousel_img_component.slick_dots_container.value_of_css_property("justify-content"))

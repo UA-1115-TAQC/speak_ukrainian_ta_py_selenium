d709c60462ea36f5c8fff2eedd5e19dff3e29cba
Alevtyna Parysta <luxor952@gmail.com>
2024-04-08 10:28:16 +0200

    upd tests

--- tests/ui/pages/club_page/sign_in_to_club_with_user_login.py
+++ tests/ui/pages/club_page/sign_in_to_club_with_user_login.py
@@ -13,25 +13,22 @@ class SignInToClubTest(LogInWithUserTestRunner):
 
     def setUp(self):
         super().setUp()
-
-    def test_sign_up_child_to_club(self):
         self.homepage.get_advanced_search_header_component().set_text_selection_search_input_field(self.CATEGORY_NAME)
         self.homepage.get_advanced_search_header_component().click_search_icon()
         self.clubs_page = ClubsPage(self.driver)
         self.clubs_page.get_club_card_list()[0].click_details_button()
+
+    def test_sign_up_child_to_club(self):
         self.club_page = ClubPage(self.driver)
         sign_in_popup = self.club_page.sign_in_to_club
         sign_in_popup.click_child_checkbox_by_index(0)
         sign_in_popup.set_comment(self.COMMENT)
         sign_in_popup.click_submit_button()
+
         self.assertIn(self.homepage.get_top_notice_message(), self.SUCCESS_REGISTRATION,
                       "After registration popup with the following text should appear " + self.SUCCESS_REGISTRATION)
 
     def test_add_child_to_list_for_sign_up_to_club(self):
-        self.homepage.get_advanced_search_header_component().set_text_selection_search_input_field(self.CATEGORY_NAME)
-        self.homepage.get_advanced_search_header_component().click_search_icon()
-        self.clubs_page = ClubsPage(self.driver)
-        self.clubs_page.get_club_card_list()[0].click_details_button()
         self.club_page = ClubPage(self.driver)
         add_child = self.club_page.sign_in_to_club.click_add_children()
         add_child.set_first_name(self.FIRST_NAME)
@@ -39,7 +36,6 @@ class SignInToClubTest(LogInWithUserTestRunner):
         add_child.set_age(self.AGE)
         add_child.click_male_gender()
         add_child.click_submit_button()
-        sign_in = self.club_page.sign_in_to_club
         children_list = self.club_page.sign_up_to_club_popup().get_children_list_names()
 
         full_name = f"{self.FIRST_NAME} {self.LAST_NAME}, {self.AGE}"
